26d49322038245cf87d2c46cae4bf92dc6ea6e3c
Yaraslau <fdyjjpasr@gmail.com>
2026-09-16 17:12:13 +0200

    feat: added global Auth guard with JWT auth strategy

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from '../users/users.module';
+import { JwtAuthGuard } from './guards/jwt-auth.guard';
+import { APP_GUARD } from '@nestjs/core';
+import { JwtStrategy } from './strategies/jwt.strategy';
 
 @Module({
   imports: [UsersModule],
   controllers: [],
-  providers: [],
+  providers: [{ provide: APP_GUARD, useClass: JwtAuthGuard }, JwtStrategy],
 })
 export class AuthModule {}
